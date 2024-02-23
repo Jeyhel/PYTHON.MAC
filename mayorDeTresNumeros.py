9c2e7b186e5dcfa2d57fbc0b725c17b1537038d1
Jeyhel <160855981+Jeyhel@users.noreply.github.com>
2024-02-23 13:59:57 -0500

    SumarDosNumeros.py

---
+++ mayorDeTresNumeros.py
@@ -0,0 +1,6 @@
+num1 = int(input("Ecriba el primer numero: "))
+num2 = int(input("Ecriba el segundo numero: "))
+num3 = int(input("Ecriba el tercer numero: "))
+mayor = max(num1, num2, num3)
+print(mayor)
+           
---
+++ sumarDosNumeros.py
@@ -0,0 +1,4 @@
+num1 = input("Escriba el primer numero: ")
+num2 = input("Esciba el seundo numero: ")
+suma = int (num1) + int (num2) 
+print (suma)
